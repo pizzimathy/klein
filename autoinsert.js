be774c82f884c395a0c7424783cc2a09f630ac94
Anthony Pizzimenti <apizzimenti@Anthonys-MacBook.local>
2023-06-25 17:28:59 -0400

    init

---
+++ autoinsert.js
@@ -0,0 +1,8 @@
+
+$(document).ready(function(){
+    // Load the header and sidebar when the page loads.
+    $("#sidebar-parent").load("sidebar.html");
+    $("#hparent").load("header.html");
+
+    // Also color the sidebar links appropriately, depending on the page.
+});
